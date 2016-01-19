deb2a1a3753441c032dcb7329bce8173c9035b52
koenchiu <koen.chiu@gmail.com>
2016-01-19 22:31:15 +0800

    在calc_Beta_v1中尝试实现表达式计算器

--- Calculator/calc_Beta_v1.c
+++ Calculator/calc_Beta_v1.c
@@ -2,32 +2,67 @@
 #include <stdlib.h>
 #include <math.h>
 
-struct TreeNode
+typedef struct UnionType
 {
-	char Element;
-	TreeNode* Right;
-	TreeNode* Left;
-	TreeNode()
-	{
-		Element = 0;
-		Right = NULL;
-		Right = NULL;
-	}
-}
+	char sign;
+	int num;
+} ElementType;
 
-int main()
+int ArrayToInfix(char* input, ElementType* infix)
 {
 	int i = 0;
-	char input[20] = {0};
-	int infix[20] = {0};
-	int suffix[20] = {0};
-	fgets(input,20,stdin);
+	int j = 0;
+	int value = 0;
 	while(input[i] != '\0')
 	{
 		while(input[i] >= '0' && input[i] <= '9')
 		{
-			int value = 0;
-			value = value*10 + atoi(input[i]);		
+			value = value * 10 + (input[i] - '0');
+			i++;			
 		}	
+		infix[j++].num = value;
+		value = 0;
+		if(input[i] != '\0')
+		{
+			infix[j++].sign = input[i];
+			i++;
+		}
 	}
+	infix[j].sign = '\0';
+	return j;
+}
+
+void PrintInfix(ElementType* infix,int len)
+{
+	int i = 0;
+	if(infix == NULL)
+		return ;
+	for(i = 0;i < len;++i)
+	{
+		if(infix[i].sign == '\0')
+		{
+			printf("%d",infix[i].num);	
+		}
+		else
+		{
+			printf(" %c ",infix[i].sign);
+		}
+	}
+
+}
+
+void InfixToSuffix(ElementType* infix, ElementType* suffix)
+{
+
+}
+
+int main()
+{
+	char input[20] = {0};
+	int lenOfInfix = 0;
+	ElementType infix[20] = {0};
+	ElementType suffix[20] = {0};
+	fgets(input,20,stdin);
+	lenOfInfix = ArrayToInfix(input,infix);
+	PrintInfix(infix,lenOfInfix);
 }
